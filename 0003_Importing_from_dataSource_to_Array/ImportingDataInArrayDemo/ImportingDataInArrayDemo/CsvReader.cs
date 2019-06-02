5e32376ec9cd35c8160621c722e61d3f81b121c5
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-06-02 13:26:13 +0530

    Theory and code for parsing a line with a bug.

--- 0003_Importing_from_dataSource_to_Array/ImportingDataInArrayDemo/ImportingDataInArrayDemo/CsvReader.cs
+++ 0003_Importing_from_dataSource_to_Array/ImportingDataInArrayDemo/ImportingDataInArrayDemo/CsvReader.cs
@@ -16,5 +16,17 @@ namespace ImportingDataInArrayDemo
             Country[] countries = new Country[nCountries];
             return countries;
         }
+
+        public Country ReadCountryFromCsvLine(string csvLine)
+        {
+            string[] parts = csvLine.Split(',');
+
+            string name = parts[0];
+            string code = parts[1];
+            string region = parts[2];
+            int population = int.Parse(parts[3]);
+
+            return new Country(name, code, region, population);
+        }
     }
 }
