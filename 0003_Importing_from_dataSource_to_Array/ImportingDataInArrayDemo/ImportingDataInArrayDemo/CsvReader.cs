63a9f568c779a9344d70a5d5a29f1d79b826ad91
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-06-03 22:16:33 +0530

    Corrected the bug and theory.

--- 0003_Importing_from_dataSource_to_Array/ImportingDataInArrayDemo/ImportingDataInArrayDemo/CsvReader.cs
+++ 0003_Importing_from_dataSource_to_Array/ImportingDataInArrayDemo/ImportingDataInArrayDemo/CsvReader.cs
@@ -35,11 +35,33 @@ namespace ImportingDataInArrayDemo
         {
             string[] parts = csvLine.Split(new char[] { ',' });
 
-            string name = parts[0];
-            string code = parts[1];
-            string region = parts[2];
-            int population = int.Parse(parts[3]);
 
+            string name;
+            string code;
+            string region;
+            string popText;
+
+            switch(parts.Length)
+            {
+                case 4:
+                    name = parts[0];
+                    code = parts[1];
+                    region = parts[2];
+                    popText = parts[3];
+                    break;
+                case 5:
+                    name = parts[0] + ", " + parts[1];
+                    name = name.Replace("\"", null).Trim();
+                    code = parts[2];
+                    region = parts[3];
+                    popText = parts[4];
+                    break;
+                default:
+                    throw new Exception($"Can't parse country fro csvLine: {csvLine}");
+            }
+
+            // TryParse leaves population=0 if it can't parse
+            int.TryParse(popText, out int population);
             return new Country(name, code, region, population);
         }
     }
