94262dedae9760ff3f20a683c4ae46d4ec095bb6
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-06-04 23:04:18 +0530

    Theory and code changes to instantiate a dictionary.

---
+++ 0005_Storing_Keyed_Data_Dictionaries/BasicDictionaryDemo/BasicDictionaryDemo/Country.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicDictionaryDemo
+{
+    public class Country
+    {
+        public int Population { get; }
+        public string Name { get; }
+        public string Region { get; }
+        public string Code { get; }
+
+        public Country(string name, string code, string region, int population)
+        {
+            Name = name;
+            Code = code;
+            Region = region;
+            Population = population;
+        }
+    }
+}
--- 0005_Storing_Keyed_Data_Dictionaries/BasicDictionaryDemo/BasicDictionaryDemo/Program.cs
+++ 0005_Storing_Keyed_Data_Dictionaries/BasicDictionaryDemo/BasicDictionaryDemo/Program.cs
@@ -10,6 +10,10 @@ namespace BasicDictionaryDemo
     {
         static void Main(string[] args)
         {
+            Country norway = new Country("Norway", "NOR", "Europe", 5_282_223);
+            Country finland = new Country("Finland", "FIN", "Europe", 5_511_383);
+
+            Dictionary<string, Country> countries = new Dictionary<string, Country>();
         }
     }
 }
