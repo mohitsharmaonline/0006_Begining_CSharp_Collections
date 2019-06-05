c0ddd0f687f6cb9d38b6229d323acb9f3a1d95c7
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-06-06 04:49:25 +0530

    Theory and code changes for Dictionary Initializers.

--- 0005_Storing_Keyed_Data_Dictionaries/BasicDictionaryDemo/BasicDictionaryDemo/Program.cs
+++ 0005_Storing_Keyed_Data_Dictionaries/BasicDictionaryDemo/BasicDictionaryDemo/Program.cs
@@ -13,12 +13,13 @@ namespace BasicDictionaryDemo
             Country norway = new Country("Norway", "NOR", "Europe", 5_282_223);
             Country finland = new Country("Finland", "FIN", "Europe", 5_511_383);
 
-            Dictionary<string, Country> countries = new Dictionary<string, Country>();
+            Dictionary<string, Country> countries = new Dictionary<string, Country>
+            {
+                { norway.Code, norway },
+                { finland.Code, finland }
+            };
 
-            countries.Add(norway.Code, norway);
-            countries.Add(finland.Code, finland);
-
-            foreach(var country in countries.Values)
+            foreach (var country in countries.Values)
             {
                 Console.WriteLine(country.Name);
             }
