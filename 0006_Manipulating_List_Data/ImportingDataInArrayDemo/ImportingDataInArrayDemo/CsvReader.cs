5474cabb8ba1355f34581af6fb11bc15cad83bdd
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-06-16 10:30:49 +0530

    Project to run foreach for demo.

---
+++ 0006_Manipulating_List_Data/ImportingDataInArrayDemo/ImportingDataInArrayDemo/CsvReader.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ImportingDataInArrayDemo
+{
+    internal class CsvReader
+    {
+        private string _csvFilePath;
+
+        public CsvReader(string _csvFilePath)
+        {
+            this._csvFilePath = _csvFilePath;
+        }
+
+        internal List<Country> ReadAllCountries()
+        {
+            List<Country> countries = new List<Country>();
+            using (StreamReader sr = new StreamReader(_csvFilePath))
+            {
+                // Read header line
+                sr.ReadLine();
+
+                string csvLine;
+                while ((csvLine = sr.ReadLine()) != null)
+                {
+                    countries.Add(ReadCountryFromCsvLine(csvLine));
+                }
+            }
+
+            return countries;
+        }
+
+        public Country ReadCountryFromCsvLine(string csvLine)
+        {
+            string[] parts = csvLine.Split(new char[] { ',' });
+
+
+            string name;
+            string code;
+            string region;
+            string popText;
+
+            switch(parts.Length)
+            {
+                case 4:
+                    name = parts[0];
+                    code = parts[1];
+                    region = parts[2];
+                    popText = parts[3];
+                    break;
+                case 5:
+                    name = parts[0] + ", " + parts[1];
+                    name = name.Replace("\"", null).Trim();
+                    code = parts[2];
+                    region = parts[3];
+                    popText = parts[4];
+                    break;
+                default:
+                    throw new Exception($"Can't parse country fro csvLine: {csvLine}");
+            }
+
+            // TryParse leaves population=0 if it can't parse
+            int.TryParse(popText, out int population);
+            return new Country(name, code, region, population);
+        }
+    }
+}
