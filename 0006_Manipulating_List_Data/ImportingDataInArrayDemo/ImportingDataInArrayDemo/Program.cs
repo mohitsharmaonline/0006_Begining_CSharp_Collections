30e590ce35cce96b2d9d6a98c154511e839de8c5
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-06-16 13:13:57 +0530

    code changes for reverse enumeration on  a collection

--- 0006_Manipulating_List_Data/ImportingDataInArrayDemo/ImportingDataInArrayDemo/Program.cs
+++ 0006_Manipulating_List_Data/ImportingDataInArrayDemo/ImportingDataInArrayDemo/Program.cs
@@ -23,16 +23,17 @@ namespace ImportingDataInArrayDemo
             }
 
             int maxToDisplay = userInput;
-            for(int i = 0; i < countries.Count; i++)
+            for(int i = countries.Count - 1; i >= 0; i--)
             {
-                if (i > 0 && (i % maxToDisplay == 0))
+                int displayIndex = countries.Count - 1 - i;
+                if (displayIndex > 0 && (displayIndex % maxToDisplay == 0))
                 {
                     Console.WriteLine("Hit return to continue, anything else to quit>");
                     if (Console.ReadLine() != "")
                         break;
                 }
                 Country country = countries[i];
-                Console.WriteLine($"{i+1}: {PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+                Console.WriteLine($"{displayIndex+1}: {PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
             }
         }
     }
