d9f01aa6955f2caa1f5f0cda2a7cb11fa5c8318e
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-06-25 20:01:25 +0530

    copied old project to make changes for Demo on limiting rows number with LINQ.

---
+++ 0007_Selecting_Items_Using_LINQ/BasicDictionaryDemo/BasicDictionaryDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicDictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\mohit\Documents\0006_Begining_CSharp_Collections\0003_Importing_from_dataSource_to_Array\data\Pop by Largest Final.csv";
+            CsvReader reader = new CsvReader(filePath);
+            Dictionary<string, Country> countries = reader.ReadAllCountries();
+
+            Console.WriteLine("Which country do you want to look up?");
+            string userInput = Console.ReadLine();
+
+            bool gotCountry = countries.TryGetValue(userInput, out Country country);
+            if(!gotCountry)
+            {
+                Console.WriteLine($"sorry, there is no country with the code, {userInput}");
+            }
+            else
+            {
+                Console.WriteLine($"{country.Name} hass population {PopulationFormatter.FormatPopulation(country.Population)}");
+            }
+        }
+    }
+}
