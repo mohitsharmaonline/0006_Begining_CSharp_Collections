0e87d056599f0f463ec227c121eda385b619200b
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-06-25 20:42:35 +0530

    Demo of ordery to sort by name.

--- 0007_Selecting_Items_Using_LINQ/ImportingDataInArrayDemo/ImportingDataInArrayDemo/Program.cs
+++ 0007_Selecting_Items_Using_LINQ/ImportingDataInArrayDemo/ImportingDataInArrayDemo/Program.cs
@@ -13,9 +13,9 @@ namespace ImportingDataInArrayDemo
             string filePath = @"C:\Users\mohit\Documents\0006_Begining_CSharp_Collections\0003_Importing_from_dataSource_to_Array\data\Pop by Largest Final.csv";
             CsvReader reader = new CsvReader(filePath);
 
-            List<Country> countries = reader.ReadAllCountries();    
+            List<Country> countries = reader.ReadAllCountries();
 
-            foreach (Country country in countries.Take(10))
+            foreach (Country country in countries.OrderBy(country => country.Name))
             {
                 Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}:" +
                     $" {country.Name}");
